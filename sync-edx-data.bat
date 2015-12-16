3733706731e0a156922bee3dae0c88ae8c790b45
Lars Andersson <lars.j.andersson@chalmers.se>
2015-12-16 18:10:48 +0100

    Take organization and backup location as arguments.

--- sync-edx-data.bat
+++ sync-edx-data.bat
@@ -1,4 +1,10 @@
 @echo off
-aws s3 sync --exclude "*" --include "chalmersx*" s3://course-data/ ./course-data/
-aws s3 sync s3://edx-course-data/chalmersx ./edx-course-data/
-xcopy . Z:\edX\ /s /e /d /y
+if [%1] == [] GOTO error
+if [%2] == [] GOTO error
+aws s3 sync --exclude "*" --include "%1*" s3://course-data/ ./course-data/
+aws s3 sync s3://edx-course-data/%1 ./edx-course-data/
+xcopy . %2 /s /e /d /y
+GOTO end
+:error
+echo You have to submit organization and backup location as arguments.
+:end
