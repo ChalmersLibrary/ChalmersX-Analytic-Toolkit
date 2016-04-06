be056c4af93e62538feb142c372c12cef7fe6579
Lars Andersson <lars.j.andersson@chalmers.se>
2016-04-06 09:01:36 +0200

    Added new video data divided into time compartments.

--- create-improved-video-data.pl
+++ create-improved-video-data.pl
@@ -35,31 +35,34 @@ if (-d $dest_dir) {
     print "\nFile with the same name as the destination directory '$dest_dir' already exists. Exiting...\n";
 } else {
     mkdir $dest_dir;
-    
+
     my %student_activity_video_keys = ();
     my %student_activity_video_data = ();
-    
+
     my %student_activity_video_ng_keys = ();
     my %student_activity_video_ng_data = ();
+
+    my %student_activity_video_ng2_keys = ();
+    my %student_activity_video_ng2_data = ();
 
     # Iterate through all the event data files and do stuff with the video data.
     my @event_data_files = grep { -f } glob $data_folder . '/events/*';
     foreach (@event_data_files) {
         if (/.*([0-9]{4}-[0-9]{2}-[0-9]{2})\.log\.gz$/) {
             my $event_date = str2time($1);
-                    
+
             print "Processing event file for date $1...\n";
-            
+
             my $gzfh = new IO::Zlib;
             $gzfh->open($_, "rb")
                 or die "Could not open file '$_' $!\n";
-                
+
             my $events_belonging_to_course = 0;
             my $events_not_belonging_to_course = 0;
             while (my $row = <$gzfh>) {
                 chomp $row;
                 my $event = JSON::XS->new->utf8->decode($row);
-                
+
                 my $event_user_id = $event->{ "context" }->{ "user_id" };
                 my $event_course_id = $event->{ "context" }->{ "course_id" };
                 my $event_event_type = $event->{ "event_type" };
@@ -70,7 +73,7 @@ if (-d $dest_dir) {
                     $event_date =~ s/-//g;
                 }
                 my $event_source = $event->{ "event_source" };
-            
+
                 if (not defined $event_user_id) {
                     $event_user_id = "undefined";
                 }
@@ -81,27 +84,58 @@ if (-d $dest_dir) {
                     $event_event_type eq "speed_change_video" || $event_event_type eq "stop_video" || $event_event_type eq "video_hide_cc_menu" ||
                     $event_event_type eq "video_show_cc_menu")
                 {
-                    $student_activity_video_keys{ $event_event_type } = 1;
-                    $student_activity_video_data{ $event_user_id }{ $event_event_type }++;
+                    my $event_name = $event->{ "name" };
+
+                    $student_activity_video_keys{ $event_name } = 1;
+                    $student_activity_video_data{ $event_user_id }{ $event_name }++;
 
                     my $video_event_event = $event->{ "event" };
                     if ($video_event_event && ref($video_event_event) ne "HASH") {
                         $video_event_event = JSON::XS->new->utf8->decode($video_event_event);
-                    }
 
-                    $student_activity_video_ng_keys{ $event_event_type } = 1;
-                    $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ "user_id" } = $event_user_id;
-                    $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ "video_event_id" } = $video_event_event->{ "id" };
-                    $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ $event_event_type }++;
+                        $student_activity_video_ng_keys{ $event_name } = 1;
+                        $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ "user_id" } = $event_user_id;
+                        $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ "video_event_id" } = $video_event_event->{ "id" };
+                        $student_activity_video_ng_data{ $event_user_id . ":" . $video_event_event->{ "id" } }{ $event_name }++;
+
+                        if (defined $video_event_event->{ "currentTime" })
+                        {
+                            my $time_compartment = int($video_event_event->{ "currentTime" } / 5); # Divide everything into 5 second (?) time compartments.
+                            $student_activity_video_ng2_keys{ $event_name } = 1;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment }{ "user_id" } = $event_user_id;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment }{ "video_event_id" } = $video_event_event->{ "id" };
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment }{ "time_compartment" } = $time_compartment;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment }{ $event_name }++;
+                        }
+                        elsif (defined $video_event_event->{ "old_time" } && defined $video_event_event->{ "new_time" })
+                        {
+                            my $time_compartment_old_time = int($video_event_event->{ "old_time" } / 5); # Divide everything into 5 second (?) time compartments.
+                            my $time_compartment_new_time = int($video_event_event->{ "new_time" } / 5); # Divide everything into 5 second (?) time compartments.
+                            my $event_type_old_time = $event_name . "_start";
+                            my $event_type_new_time = $event_name . "_end";
+
+                            $student_activity_video_ng2_keys{ $event_type_old_time } = 1;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_old_time }{ "user_id" } = $event_user_id;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_old_time }{ "video_event_id" } = $video_event_event->{ "id" };
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_old_time }{ "time_compartment" } = $time_compartment_old_time;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_old_time }{ $event_type_old_time }++;
+
+                            $student_activity_video_ng2_keys{ $event_type_new_time } = 1;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_new_time }{ "user_id" } = $event_user_id;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_new_time }{ "video_event_id" } = $video_event_event->{ "id" };
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_new_time }{ "time_compartment" } = $time_compartment_new_time;
+                            $student_activity_video_ng2_data{ $event_user_id . ":" . $video_event_event->{ "id" } . ":" . $time_compartment_new_time }{ $event_type_new_time }++;
+                        }
+                    }
                 }
             }
         }
     }
-    
+
     # Build video activity table with aggregated data.
     my $filename = "$dest_dir/video_activity.csv";
     open (my $va_fh, '>', $filename) or die "Failed to open file '$filename' for writing.";
-    
+
     my $csv_first_line = "user_id,";
     foreach my $key (keys %student_activity_video_keys) {
         $csv_first_line .= "$key,";
@@ -122,11 +156,11 @@ if (-d $dest_dir) {
         chop($video_activity_data_record);
         print $va_fh "$video_activity_data_record\n";
     }
-    
+
     # Build video activity ng table with aggregated data.
     $filename = "$dest_dir/video_activity_ng.csv";
     open (my $vang_fh, '>', $filename) or die "Failed to open file '$filename' for writing.";
-    
+
     $csv_first_line = "user_id,video_event_id,";
     foreach my $key (keys %student_activity_video_ng_keys) {
         $csv_first_line .= "$key,";
@@ -143,10 +177,36 @@ if (-d $dest_dir) {
             } else {
                 $video_activity_data_record .= ",";
             }
-            
+
         }
         chop($video_activity_data_record);
         print $vang_fh "$video_activity_data_record\n";
+    }
+
+    # Build video activity ng2 table with aggregated data.
+    $filename = "$dest_dir/video_activity_ng2.csv";
+    open (my $vang2_fh, '>', $filename) or die "Failed to open file '$filename' for writing.";
+
+    $csv_first_line = "user_id,video_event_id,time_compartment,";
+    foreach my $key (keys %student_activity_video_ng2_keys) {
+        $csv_first_line .= "$key,";
+    }
+    chop($csv_first_line);
+    print $vang2_fh "$csv_first_line\n";
+
+    foreach my $data (values %student_activity_video_ng2_data) {
+        my $video_activity_data_record = %{ $data }{ "user_id" } . "," . %{ $data }{ "video_event_id" } . "," . %{ $data }{ "time_compartment" } . ",";
+        foreach my $activity_type (keys %student_activity_video_ng2_keys) {
+            my $activity_data = %{ $data }{ $activity_type };
+            if ($activity_data) {
+                $video_activity_data_record .= $activity_data . ",";
+            } else {
+                $video_activity_data_record .= ",";
+            }
+
+        }
+        chop($video_activity_data_record);
+        print $vang2_fh "$video_activity_data_record\n";
     }
 
     print "Done.\n\n";
